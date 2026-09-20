81fb852bbe19685d4f5b40f98597674f89005548
Katie McLaughlin <katie@glasnt.com>
2026-09-20 13:20:57 +1000

    add new 2026 exports, in case formats changed

--- tests/test_render.py
+++ tests/test_render.py
@@ -13,6 +13,10 @@ SAMPLES = ["google-slides", "canva", "microsoft-powerpoint"]
 SLIDES_FN = "sample-slides.pdf"
 NOTES_FN = "sample-notes.pdf"
 
+# 2026 exports
+SLIDES_FN_2026 = "sample-slides_2026.pdf"
+NOTES_FN_2026 = "sample-notes_2026.pdf"
+
 GENERATED_FOLDER = Path("pytest_data")
 
 
@@ -34,6 +38,35 @@ def test_slides_type(sample_fn):
             Path(input_folder) / SLIDES_FN,
             "-n",
             Path(input_folder) / NOTES_FN,
+            "-o",
+            output_folder,
+        ],
+    )
+    assert result.exit_code == 0
+    assert output_folder.exists()
+    assert (output_folder / "index.html").exists()
+
+    check_html_attributes(output_folder / "index.html")
+
+
+@pytest.mark.parametrize("sample_fn", SAMPLES)
+def test_slides_type_2026(sample_fn):
+    runner = CliRunner()
+
+    output_folder = GENERATED_FOLDER / sample_fn
+
+    if Path(output_folder).exists():
+        shutil.rmtree(output_folder)
+
+    input_folder = Path(SAMPLE_FOLDER) / sample_fn
+
+    result = runner.invoke(
+        cli,
+        [
+            "-s",
+            Path(input_folder) / SLIDES_FN_2026,
+            "-n",
+            Path(input_folder) / NOTES_FN_2026,
             "-o",
             output_folder,
         ],
